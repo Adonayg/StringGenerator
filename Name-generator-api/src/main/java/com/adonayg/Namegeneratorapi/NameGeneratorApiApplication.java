d5d4e94763429833e350d074374323555da0b6bc
Adonayg <adonay@tutanota.com>
2018-12-04 09:35:06 +0000

    First commit

---
+++ Name-generator-api/src/main/java/com/adonayg/Namegeneratorapi/NameGeneratorApiApplication.java
@@ -0,0 +1,12 @@
+package com.adonayg.Namegeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameGeneratorApiApplication.class, args);
+	}
+}
---
+++ Name-generator-api/src/test/java/com/adonayg/Namegeneratorapi/NameGeneratorApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adonayg.Namegeneratorapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NameGeneratorApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
